75a7fac2b23e7ef982f07c2e802d0ab3536400f5
Arman Eskandari <71627457+ArmanEskandari@users.noreply.github.com>
2022-01-27 09:54:56 +0330

    navbar: - changed style - added authors link

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -2,38 +2,53 @@ import * as React from "react";
 import {NavLink} from "react-router-dom";
 
 export function NavList() {
-    // This styling will be applied to a <NavLink> when the
-    // route that it links to is currently selected.
-    let activeStyle = {
-        textDecoration: "underline"
-    };
-
-    let activeClassName = "underline"
 
     return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink
-                        to="/"
-                        style={({isActive}) =>
-                            isActive ? activeStyle : {}
-                        }
-                    >
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="authors"
-                        className={({isActive}) =>
-                            isActive ? activeClassName : ''
-                        }
-                    >
-                        Books
-                    </NavLink>
-                </li>
-            </ul>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Library</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"/>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/"
+                                    className={({isActive}) =>
+                                        isActive ? 'active' : 'nav-link'
+                                    }
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    to="books"
+                                    className={({isActive}) =>
+                                        isActive ? "active" : 'nav-link'
+                                    }
+                                >
+                                    Books
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    to="authors"
+                                    className={({isActive}) =>
+                                        isActive ? "active" : 'nav-link'
+                                    }
+                                >
+                                    authors
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </nav>
     );
 }
